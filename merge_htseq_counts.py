f951cf6fbec22fe98012e37a6dae1cf2b1190670
Rudramani Pokhrel <rudrap042@gmail.com>
2022-04-06 16:08:19 -0600

    Add files via upload

---
+++ merge_htseq_counts.py
@@ -0,0 +1,36 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Script to merge htseq-count data form multiple samples')
+
+parser.add_argument("Run this script in the folder contaning count data")
+
+args = parser.parse_args()
+
+print(args.print_string)
+import pandas as pd
+import glob
+print("Merging started")
+def merge_count(input_files = "*txt"):
+    files = glob.glob(input_files)
+    L = []
+    for file in files:
+        print(f"reading file: {file}")
+        try:
+            f = pd.read_csv(file,sep = "\s+", header = None, index_col = 0)
+            f.columns = [file]
+            L.append(f)
+        except:
+            pass
+    df = pd.concat(L)
+    df = df.fillna(0)
+    discard = []
+    for i in df.index:
+        if  i.startswith("_"):
+            discard.append(i)
+    df = df.drop(discard)
+    df =df.groupby(df.index).mean()
+    df.columns = [i[0:-4] for i in df.columns]
+    return df
+df = merge_count("htseq_result/*txt")
+df.to_csv("merged_counts.csv")
+print("Data merged succesfully")
